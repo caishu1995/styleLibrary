1acb7e445fc1097ea96427dfdb3de35e59423863
caishu1995 <2687366516@qq.com>
2018-09-05 12:04:35 +0800

    11.1.1.1 提交等待画面1

---
+++ 11.Wait/waitInit.js
@@ -0,0 +1,4 @@
+/* 等待画面1 */
+var waitStyle1 = function(text, DOMId) {
+    $("#" + DOMId).html('<div class="text">' + text + '</div><div class="circle"></div><div class="circle"></div><div class="circle"></div>');
+};
