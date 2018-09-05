0d958262f77ab7f32879eaa568374afa58d3dc4c
caishu1995 <2687366516@qq.com>
2018-09-05 15:45:20 +0800

    2.1.1.1 添加按钮样式

---
+++ 2.Button/Button.js
@@ -0,0 +1,7 @@
+/* 按钮样式1 */
+var button1 = function(DOMId) {
+    $("#" + DOMId).html("<div class=\"h-content-buttons\">\n" +
+        "            <button><span></span><span><svg><path d=\"M14,0.7L13.3,0L4.7,8.3l0,0L4,9l0,0l0.7,0.7l0,0l8.5,8.3l0.7-0.7L5.4,9L14,0.7z\"></path></svg></span></button>" +
+        "            <button><span></span><span><svg><path d=\"M12.6,9L4,17.3L4.7,18l8.5-8.3l0,0L14,9l0,0l-0.7-0.7l0,0L4.7,0L4,0.7L12.6,9z\"></path></svg></span></button>" +
+        "        </div>");
+};
